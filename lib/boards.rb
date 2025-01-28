2072237c9479bb9b11af63c4fb43ad80fb1faba1
acskii <andrew.sameh.06@gmail.com>
2025-01-28 11:30:54 +0200

    fix: code cleaner

--- lib/boards.rb
+++ lib/boards.rb
@@ -1,64 +1,64 @@
 require_relative 'pegs'
 
 module Boards
-    class Board
-        def initialize
-            @pegs = []
+  class Board
+    def initialize
+      @pegs = []
 
-            4.times do |index|
-                @pegs << PlayPeg.new(:neutral, index)
-            end
-        end
-
-        def pegs
-            @pegs.map { |peg| peg.color }
-        end
-
-        def color_pegs(*colors)
-            return false unless len(colors) == len(@pegs)
-
-            colors.each_with_index do |color, index|
-                @pegs[index].edit_color(color)
-            end
-        end
+      4.times do |index|
+        @pegs << PlayPeg.new(:neutral, index)
+      end
     end
 
-    class GameBoard < Board
-        attr_reader :hints
-
-        def initialize
-            super
-            @hints = []
-        end
-
-        def add_hints(red, white)
-            return false unless [red, white].all? { |n| n.instance_of? Integer }
-
-            red.times { |_| @hints << HintPeg.new('c') }
-            white.times { |_| @hints << HintPeg.new('w') }
-            true
-        end
+    def pegs
+      @pegs.map { |peg| peg.color }
     end
 
-    class CodeBoard < Board
-        def initialize(*colors)
-            super()
+    def color_pegs(*colors)
+      return false unless len(colors) == len(@pegs)
 
-            color_pegs(*colors)
-        end
-
-        def hints_with(game_board)
-            hints = [0,0]
-
-            len(@pegs).times do |index|
-                if pegs[index] == game_board.pegs[index]
-                    hints[0] += 1
-                else
-                    hints[1] += 1 if pegs.include? game_board.pegs[index]
-                end
-            end
-
-            hints
-        end
+      colors.each_with_index do |color, index|
+        @pegs[index].edit_color(color)
+      end
     end
+  end
+
+  class GameBoard < Board
+    attr_reader :hints
+
+    def initialize
+      super
+      @hints = []
+    end
+
+    def add_hints(red, white)
+      return false unless [red, white].all? { |n| n.instance_of? Integer }
+
+      red.times { |_| @hints << HintPeg.new('c') }
+      white.times { |_| @hints << HintPeg.new('w') }
+      true
+    end
+  end
+
+  class CodeBoard < Board
+    def initialize(*colors)
+      super()
+
+      color_pegs(*colors)
+    end
+
+    def hints_with(game_board)
+      hints = [0, 0]
+
+      len(@pegs).times do |index|
+        if pegs[index] == game_board.pegs[index]
+          hints[0] += 1
+        elsif pegs.include? game_board.pegs[index]
+          hints[1] += 1
+        end
+      end
+
+      hints
+    end
+  end
 end
