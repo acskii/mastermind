9e658f2879a9f90f51fd2455c66d79d4f0b09315
acskii <andrew.sameh.06@gmail.com>
2025-01-29 17:44:52 +0200

    feat: new print methods
    clear console method
    print instructions method

--- lib/print.rb
+++ lib/print.rb
@@ -1,4 +1,5 @@
 require 'colorize'
+require 'io/console'
 
 module Print
   def self.print_options(msg, options)
@@ -25,8 +26,35 @@ module Print
 
   def self.print_pegs(*colors)
     colors.each do |color|
-      print '_'.colorize(color) unless color.nil?
+      print '◉ '.colorize(color) unless color.nil?
     end
+    puts ''
+  end
+
+  def self.clear_console
+    $stdout.clear_screen
+  end
+
+  def self.print_title
+    puts('WELCOME TO'.center(20, ' ').colorize(:light_white))
+    puts('🄼 🄰 🅂 🅃 🄴 🅁 🄼 🄸 🄽 🄳'.center(20, ' ').colorize(:light_white))
+    puts('=' * 20)
+    puts ''
+
+    puts('HOW TO PLAY'.center(20, ' ').colorize(:light_white))
+    puts('=' * 20)
+    puts('CREATOR CREATES A COLOR CODE'.center(20, ' ').colorize(:light_white))
+    puts('BREAKER GUESSES A COLOR CODE'.center(20, ' ').colorize(:light_white))
+    puts('CREATOR HINTS IF GUESS IS CLOSE'.center(20, ' ').colorize(:light_white))
+    puts('BREAKER USES HINTS TO CRACK CODE'.center(20, ' ').colorize(:light_white))
+    puts('WHO WILL LAUGH IN THE END!!'.center(20, ' ').colorize(:light_white))
+    puts ''
+
+    puts('HINTS'.center(20, ' ').colorize(:light_white))
+    puts('=' * 20)
+    puts('RED PEG   | ◉ | => Correct peg'.center(20, ' ').colorize(:light_red))
+    puts('WHITE PEG | ◉ | => Correct peg but wrong position'.center(20, ' ').colorize(:light_white))
+    puts('NONE      |   | => Wrong'.center(20, ' ').colorize(:white))
     puts ''
   end
 end
