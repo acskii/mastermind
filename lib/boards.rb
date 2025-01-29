6f7d2829b3fde1be8f3434bdc8d37c0542de4265
acskii <andrew.sameh.06@gmail.com>
2025-01-29 17:48:38 +0200

    fix: bug fixes in board objects
    hints_with now works according to game rules.
    .. white pegs are added if a peg isn't
    ,, previously chosen as correct
    
    replaced len() with .length -> python confusion :p
    mention of peg objects now works from module
    add same_as? method to compare code board with
    .. game board passed, true if both are the same.

--- lib/boards.rb
+++ lib/boards.rb
@@ -2,20 +2,18 @@ require_relative 'pegs'
 
 module Boards
   class Board
+    attr_reader :pegs
+
     def initialize
       @pegs = []
 
       4.times do |index|
-        @pegs << PlayPeg.new(:neutral, index)
+        @pegs << Pegs::PlayPeg.new(:neutral, index)
       end
     end
 
-    def pegs
-      @pegs.map { |peg| peg.color }
-    end
-
-    def color_pegs(*colors)
-      return false unless len(colors) == len(@pegs)
+    def color_pegs(colors)
+      return false unless colors.length == @pegs.length
 
       colors.each_with_index do |color, index|
         @pegs[index].edit_color(color)
@@ -34,28 +32,40 @@ module Boards
     def add_hints(red, white)
       return false unless [red, white].all? { |n| n.instance_of? Integer }
 
-      red.times { |_| @hints << HintPeg.new('c') }
-      white.times { |_| @hints << HintPeg.new('w') }
+      red.times { |_| @hints << Pegs::HintPeg.new('c') }
+      white.times { |_| @hints << Pegs::HintPeg.new('w') }
       true
     end
   end
 
   class CodeBoard < Board
-    def initialize(*colors)
+    def initialize(colors)
       super()
 
-      color_pegs(*colors)
+      color_pegs(colors)
+    end
+
+    def same_as?(game_board)
+      hints_with(game_board)[0] == 4
     end
 
     def hints_with(game_board)
       hints = [0, 0]
+      correct = []
 
-      len(@pegs).times do |index|
-        if pegs[index] == game_board.pegs[index]
+      @pegs.length.times do |index|
+        if pegs[index].color == game_board.pegs[index].color
+          correct << game_board.pegs[index].color
           hints[0] += 1
-        elsif pegs.include? game_board.pegs[index]
-          hints[1] += 1
         end
+      end
+
+      @pegs.length.times do |index|
+        next unless pegs.map do |peg|
+          peg.color
+        end.include?(game_board.pegs[index].color) && !correct.include?(game_board.pegs[index].color)
+
+        hints[1] += 1
       end
 
       hints
