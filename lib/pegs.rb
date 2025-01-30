82e1c539db6327dc53e34ff65544e9bc2fed5440
acskii <andrew.sameh.06@gmail.com>
2025-01-30 14:05:23 +0200

    fix: Peg objects are no more
    pegs are represented by characters now.
    
    colors modularized in colors module
    
    Pegs has methods to validate a valid peg and hint peg, valid peg sequence.
    
    Using .bin file to obtain all possible guesses fast.
    Method to generate file is available too.

--- lib/pegs.rb
+++ lib/pegs.rb
@@ -1,54 +1,55 @@
+require_relative 'colors'
+
 module Pegs
-  class PlayPeg
-    attr_reader :color
+  LOCATION_BIN = '../mastermind/seq.bin'
 
-    COLORS = {
-      red: :light_red,
-      cyan: :cyan,
-      blue: :blue,
-      green: :light_green,
-      yellow: :yellow,
-      purple: :magenta,
-      neutral: :light_white
-    }
+  def self.valid_peg?(peg)
+    return false unless peg.instance_of? String
 
-    def initialize(color, index)
-      color_code = COLORS.fetch(color)
-      @color = color_code || COLORS[:neutral]
+    !Colors::COLORS.dig(peg).to_s.empty?
+  end
 
-      @index = index.to_i
-    end
+  def self.valid_hint?(peg)
+    valid_peg?(peg) && Colors.get_hint_colors.include?(peg)
+  end
 
-    def edit_color(color)
-      color_code = COLORS.fetch(color)
-      if color_code
-        @color = color_code
-        true
-      end
-      false
+  def self.get_hint_meaning(hint)
+    return [] unless valid_hint?(hint)
+    return [true, false] unless Colors.get_hint_colors[0] == hint
+
+    [false, true] unless Colors.get_hint_colors[1] == hint
+  end
+
+  def self.valid_seq?(seq)
+    return false unless seq.instance_of?(String) && seq.length == 4
+
+    seq.chars.all? do |c|
+      valid_peg?(c)
     end
   end
 
-  class HintPeg
-    attr_reader :color, :answer_correct, :wrong_pos
+  def self.get_permutations
+    return nil unless File.exist?(LOCATION_BIN)
 
-    COLORS = {
-      red: :red,
-      white: :white
-    }
-
-    def initialize(type)
-      return unless %w[c w].any? { |t| t == type.downcase }
-
-      if type.downcase == 'c'
-        @answer_correct = true
-        @wrong_pos = false
-        @color = COLORS[:red]
-      else
-        @answer_correct = false
-        @wrong_pos = true
-        @color = COLORS[:white]
-      end
+    seq = []
+    File.open(LOCATION_BIN, 'r') do |f|
+      seq << f.read(4) until f.eof?
     end
+    seq
+  end
+
+  def self.generate_permutations
+    return true if File.exist? LOCATION_BIN
+
+    seq = Colors.get_play_colors.permutation(4).map { |p| p.join('') }
+
+    begin
+      File.open(LOCATION_BIN, 'w') do |f|
+        seq.each { |s| f.write(s) }
+      end
+    rescue IOError
+      false
+    end
+    true
   end
 end
